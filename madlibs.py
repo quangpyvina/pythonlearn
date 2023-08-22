5141cbe9e9d00105a6e8c9694899d7a74dc387fb
quangpyvina <quangpyvina@gmail.com>
2023-08-22 08:33:37 +0700

    python fire firest

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# word = input("Channel_name: ")
+# madlib = "Subcribe to {Channel_name} " .format(Channel_name=word)
+# print(madlib)
+adj = input("Adjective:  ")
+verb1 = input("Verb:  ")
+verb2 = input("Verb:  ")
+famous_person = input("Famous person:  ")
+
+madlib = "Computer programming is so {adj}! It makes me so excited all \
+the time because I love to {verb1}. Stay hydrated and {verb2} like you are \
+{famous_person}".format(adj=adj, verb1=verb1, verb2=verb2, famous_person=famous_person)
+
+print(madlib)
