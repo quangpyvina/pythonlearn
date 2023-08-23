cd913ba558dcc205ea011a2fb1aec563a81b2584
quangpyvina <quangpyvina@gmail.com>
2023-08-23 10:09:54 +0700

    Random

--- selector.py
+++ selector.py
@@ -14,7 +14,7 @@ foods = [
 # random.randint(1, 5) --> random number between 1 and 5
 # random.choice(array) --> random item in this array
 
-selected = random.choice(foods) # randomly choose a food for the lunch
+selected = random.choice(foods) # randomly choose a food for the lunch at PY VINA
 
 print('You should eat the' )
 print(selected)
