c1dce9ee71f516fa00996cda358c450fae826484
quangpyvina <quangpyvina@gmail.com>
2023-08-23 10:08:10 +0700

    Create selector.py

---
+++ selector.py
@@ -0,0 +1,20 @@
+# Random selection python by VanQuang
+
+import random
+
+foods = [
+    'Bun rieu',
+    'Com',
+    'Ca',
+    'Cua',
+    'Tom'
+
+]
+
+# random.randint(1, 5) --> random number between 1 and 5
+# random.choice(array) --> random item in this array
+
+selected = random.choice(foods) # randomly choose a food for the lunch
+
+print('You should eat the' )
+print(selected)
